36433b347809b75be47e9028cae1e3d0435375ce
Jon <jonpkehoe@outlook.com>
2023-04-26 16:54:34 +0100

    commit3

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -1,10 +1,18 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
+import processing.data.Table;
+import processing.data.TableRow;
 
 import processing.core.PApplet;
 
 public class DANI extends PApplet {
+
+	private ArrayList<Word> model;
+
+	public DANI() {
+
+	}
 
 	
 
@@ -40,5 +48,29 @@ public class DANI extends PApplet {
 		textSize(20);
         textAlign(CENTER, CENTER);
         
+	}
+
+	public Word findWord(String str) {
+        for (Word word : model) {
+            if (word.getWord().equals(str)) {
+                return word;
+            }
+		}
+		return null;
+		
+	}
+	
+	
+	
+
+	public void loadFile() {
+		String[] lines = loadStrings("small.txt");
+
+        for (String line : lines) {
+            String[] words = split(line, ' ');
+
+		}
+		
+
 	}
 }
